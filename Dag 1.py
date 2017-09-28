fddcc83fe369702df8cfbb352b2fb5f840b3c126
lodeh <lode.hillaert@student.howest.be>
2017-09-28 09:53:46 +0200

    hoi

---
+++ Dag 1.py
@@ -0,0 +1,22 @@
+# commentaarlijn
+# variabele x wordt 3
+# python return is einde van een statement
+
+
+#voornaam = input ("voornaam: ")
+#achternaam = input ("achternaam: ")
+#leeftijd= input ("leeftijd: ")
+
+
+
+#dag= int(input("geen het aantal dagen: "))
+#uren= int(input("geef het aantal dagen: "))
+#minuten= int(input("geef het aantal minuten: "))
+#seconden= int(input("geef het aantal seconden: "))
+#
+#dsec= (dag*60*60*24)
+#usec= (uren*60*60)
+#msec= (minuten*60)
+#
+#tsec= (dsec+seconden+usec+msec)
+#print("Het totaal aantal seconden is: {0}".format(tsec))
---
+++ Les 2.py
@@ -0,0 +1,3 @@
+print("hello git")
+print("how are you doing")
+
